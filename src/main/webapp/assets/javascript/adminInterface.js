8b6e7364a8621024c624453e86bd51568841e979
David Hutchinson <david@scrumconnect.com>
2016-07-29 16:41:17 +0100

    removed commented out code

--- src/main/webapp/assets/javascript/adminInterface.js
+++ src/main/webapp/assets/javascript/adminInterface.js
@@ -20,10 +20,6 @@ var adminInterface = (function () {
               var components = page.find("[data-tag-type]").not("[data-tag-type='page']").not("[data-tag-nested='true']");
               _sortVertically(components);
 
-//              var insertAfterComponent = components.slice(3,4);
-//              droppedComponent.insertAfter(insertAfterComponent);
-//              droppedComponent.css('top', 'auto').css('left', 'auto');
-          
               var top = ui.offset.top;
               console.log("dropped at " + top);
               for(var index = 0; index < components.length; index++) {
@@ -49,28 +45,6 @@ var adminInterface = (function () {
               }
               console.log('done moving component');
           }
-//          drop: function( event, ui ) {
-//              // rearrange the components so that they are in a specific order, so reset the horiziontal
-//              // offset and move everything else to fit
-//              var that = $(this);
-//              var page = that.closest("[data-tag-type='page']");
-//              var components = page.find("[data-tag-type]").not("[data-tag-type='page']").not("[data-tag-nested='true']");
-//              _sortVertically(components);
-//              
-//              var top = ui.offset.top;
-//              for(var index = 0; index < components.length; index++) {
-//                  var component = $(components[index]);
-//                  var componentName = component.find('label').text().trim();
-//                  var componentTop = component.position().top;
-//                  if(component != that && componentTop > top) {
-//                      // the dropped component has been dropped below this one, so insert it here
-//                      that.insertBefore(component);
-//                      // reset the css position
-//                      that.css('top', 'auto').css('left', 'auto');
-//                      break;
-//                  }
-//              }
-//          }
         });
     };
 
