b8758fee11a192acd0eeecc89fc066fde752541c
Colin Greenwood <colingreenwood@scrumconnect.com>
2016-12-29 12:42:57 +0000

    CADS-1708 : Assisted decision. Fix tests for Claim Submit / XML Builder.

--- src/test/java/uk/gov/dwp/carersallowance/xml/ClaimAssistedDecisionTest.java
+++ src/test/java/uk/gov/dwp/carersallowance/xml/ClaimAssistedDecisionTest.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.dwp.carersallowance.controller.XmlBuilder;
 import uk.gov.dwp.carersallowance.utils.xml.ClaimXmlUtil;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,7 +16,8 @@ import static org.junit.Assert.assertThat;
 public class ClaimAssistedDecisionTest {
     private static final Logger LOG = LoggerFactory.getLogger(ClaimHeaderItemsTest.class);
 
-    private XmlBuilder xmlBuilder;
+    private Map<String, Object> sessionMap;
+
     private String appVersion = "3.14";
     private String xmlVersion = "0.27";
     private String transactionId = "16011234";
@@ -24,7 +26,7 @@ public class ClaimAssistedDecisionTest {
 
     @Before
     public void setUp() throws Exception {
-        Map<String, Object> sessionMap = new HashMap<>();
+        sessionMap = new HashMap<>();
         sessionMap.put("transactionId", transactionId);
         sessionMap.put("transactionIdAttr", transactionId);
         sessionMap.put("dateTimeGenerated", ClaimXmlUtil.currentDateTime("dd-MM-yyyy HH:mm"));
@@ -32,16 +34,24 @@ public class ClaimAssistedDecisionTest {
         sessionMap.put("appVersion", appVersion);
         sessionMap.put("origin", origin);
         sessionMap.put("language", language);
-
-        xmlBuilder = new XmlBuilder("DWPBody", sessionMap);
     }
 
     @Test
-    public void checkClaimXmlContainsAssistedDecisionReason() {
-        String reason=xmlBuilder.getNodeValue("/DWPBody/DWPCATransaction/DWPCAClaim/AssistedDecisions/AssistedDecision/Reason");
-        String decision=xmlBuilder.getNodeValue("/DWPBody/DWPCATransaction/DWPCAClaim/AssistedDecisions/AssistedDecision/Decision");
-        assertThat(reason, is("Send DS790/790B COMB to customer."));
-        assertThat(decision, is("Send DS790/790B COMB to customer."));
+    public void checkClaimXmlContainsDefaultAssistedDecisionReason() throws Exception{
+        XmlBuilder xmlBuilder = new XmlBuilder("DWPBody", sessionMap);
+        String reason = xmlBuilder.getNodeValue("/DWPBody/DWPCATransaction/DWPCAClaim/AssistedDecisions/AssistedDecision/Reason");
+        String decision = xmlBuilder.getNodeValue("/DWPBody/DWPCATransaction/DWPCAClaim/AssistedDecisions/AssistedDecision/RecommendedDecision");
+        assertThat(reason, is("Check CIS for benefits. Send Pro517 if relevant."));
+        assertThat(decision, is("Potential award,show table"));
+    }
 
+    @Test
+    public void checkClaimXmlContainsAFIPAssistedDecisionReason() throws Exception{
+        sessionMap.put("benefitsAnswer", "AFIP");
+        XmlBuilder xmlBuilder = new XmlBuilder("DWPBody", sessionMap);
+        String reason = xmlBuilder.getNodeValue("/DWPBody/DWPCATransaction/DWPCAClaim/AssistedDecisions/AssistedDecision/Reason");
+        String decision = xmlBuilder.getNodeValue("/DWPBody/DWPCATransaction/DWPCAClaim/AssistedDecisions/AssistedDecision/RecommendedDecision");
+        assertThat(reason, is("Assign to AFIP officer on CAMLite workflow."));
+        assertThat(decision, is("None,show table"));
     }
 }
