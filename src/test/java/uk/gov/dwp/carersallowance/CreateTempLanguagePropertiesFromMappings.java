52b64809716d0c0e862354e3b261bd96639b12c4
Peter Whitehead <peter@scrumconnect.com>
2017-02-06 09:31:36 +0000

    CADS-1735 : breaks in care

--- src/test/java/uk/gov/dwp/carersallowance/CreateTempLanguagePropertiesFromMappings.java
+++ src/test/java/uk/gov/dwp/carersallowance/CreateTempLanguagePropertiesFromMappings.java
@@ -1,7 +1,6 @@
 package uk.gov.dwp.carersallowance;
 
 import org.apache.commons.lang3.StringUtils;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import uk.gov.dwp.carersallowance.utils.LoadFile;
@@ -18,35 +17,35 @@ import java.util.stream.Collectors;
 /**
  * Created by peterwhitehead on 24/01/2017.
  */
-public class CreateTempWelshPropertiesFromMappings {
-    private static final Logger LOG = LoggerFactory.getLogger(CreateTempWelshPropertiesFromMappings.class);
+public class CreateTempLanguagePropertiesFromMappings {
+    private static final Logger LOG = LoggerFactory.getLogger(CreateTempLanguagePropertiesFromMappings.class);
 
     private List<String[]> mappingsScalaJava;
     private List<String> englishFile;
 
-    public CreateTempWelshPropertiesFromMappings() throws Exception {
-        englishFile = LoadFile.readLines(CreateTempWelshPropertiesFromMappings.class.getClassLoader().getResource("messages_en.properties"));
-        List<String> mappings = LoadFile.readLines(CreateTempWelshPropertiesFromMappings.class.getClassLoader().getResource("scalaJavaTextMappings.txt"));
+    public CreateTempLanguagePropertiesFromMappings() throws Exception {
+        englishFile = LoadFile.readLines(CreateTempLanguagePropertiesFromMappings.class.getClassLoader().getResource("messages_en.properties"));
+        List<String> mappings = LoadFile.readLines(CreateTempLanguagePropertiesFromMappings.class.getClassLoader().getResource("scalaJavaTextMappings.txt"));
         mappingsScalaJava = mappings.stream().filter(s -> {String s2 = s.trim(); return StringUtils.isNotBlank(s2) && !s2.startsWith("#");}).map(s -> {String[] s2 = s.split("=", 2); return new String[]{s2[0].trim(), s2[1].trim()};}).collect(Collectors.toList());
     }
 
-    private void createTempClaimWelshFile() throws Exception {
-        //get all welsh values in scala properties file
-        List<File> filesInFolder = Files.walk(Paths.get("../ClaimCapture/c3/conf/cy")).filter(Files::isRegularFile).map(Path::toFile).collect(Collectors.toList());
+    private void createTempClaimLanguageFile(final String language) throws Exception {
+        //get all language for language  values in scala properties file
+        List<File> filesInFolder = Files.walk(Paths.get("../ClaimCapture/c3/conf/" + language)).filter(Files::isRegularFile).map(Path::toFile).collect(Collectors.toList());
         List<List<String>> temp = filesInFolder.stream().map(f -> { try { return LoadFile.readLines((new File(f.getAbsolutePath())).toURL()); } catch (Exception e) { LOG.error("Exception e", e); return null; } } ).collect(Collectors.toList());
-        List<String> welshFilesValues = temp.stream().flatMap(List::stream).filter(s -> {String s2 = s.trim(); return StringUtils.isNotBlank(s2) && !s2.startsWith("#");}).collect(Collectors.toList());
+        List<String> filesValues = temp.stream().flatMap(List::stream).filter(s -> {String s2 = s.trim(); return StringUtils.isNotBlank(s2) && !s2.startsWith("#");}).collect(Collectors.toList());
 
         //create temp welsh properties file
-        final String file = "src/test/resources/temp_cy.properties";
+        final String file = "src/test/resources/temp_" + language + ".properties";
         try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(file))) {
             englishFile.forEach(line -> {
                 final String[] lineValues = line.split("=", 2);
                 try {
                     final String[] scalaJava = findMappingInScalaJava(lineValues[0].trim(), mappingsScalaJava);
                     if (scalaJava != null) {
-                        String[] welshLine = findLineInWelsh(scalaJava[0].trim(), welshFilesValues);
-                        if (welshLine != null) {
-                            writer.write(scalaJava[1].trim() + " = " + welshLine[1].trim().replace("''", "&apos;") + System.lineSeparator());
+                        String[] newLine = findLineInFile(scalaJava[0].trim(), filesValues);
+                        if (newLine != null) {
+                            writer.write(scalaJava[1].trim() + " = " + newLine[1].trim().replace("''", "&apos;") + System.lineSeparator());
                         }
                     } else {
                         writer.write(line + System.lineSeparator());
@@ -67,11 +66,11 @@ public class CreateTempWelshPropertiesFromMappings {
         return null;
     }
 
-    private String[] findLineInWelsh(final String value, final List<String> welshFilesValues) {
-        for (final String welshValue : welshFilesValues) {
-            final String[] welshValues = welshValue.split("=", 2);
-            if (welshValues[0].trim().equals(value)) {
-                return welshValues;
+    private String[] findLineInFile(final String value, final List<String> filesValues) {
+        for (final String fileValue : filesValues) {
+            final String[] newFileValues = fileValue.split("=", 2);
+            if (newFileValues[0].trim().equals(value)) {
+                return newFileValues;
             }
         }
         return null;
@@ -79,8 +78,11 @@ public class CreateTempWelshPropertiesFromMappings {
 
     public static void main(String[] args) {
         try {
-            CreateTempWelshPropertiesFromMappings createTempWelshPropertiesFromMappings = new CreateTempWelshPropertiesFromMappings();
-            createTempWelshPropertiesFromMappings.createTempClaimWelshFile();
+            CreateTempLanguagePropertiesFromMappings createTempLanguagePropertiesFromMappings = new CreateTempLanguagePropertiesFromMappings();
+            //create temp welsh language file
+            createTempLanguagePropertiesFromMappings.createTempClaimLanguageFile("cy");
+            //create temp english language file
+            createTempLanguagePropertiesFromMappings.createTempClaimLanguageFile("en");
         } catch (Exception e) {
             LOG.error("Failed to create mapping file.", e);
         }
@@ -88,7 +90,7 @@ public class CreateTempWelshPropertiesFromMappings {
 
 //    @Test
 //    public void createTestTempClaimWelshFile() throws Exception {
-//        CreateTempWelshPropertiesFromMappings createTempWelshPropertiesFromMappings = new CreateTempWelshPropertiesFromMappings();
+//        CreateTempLanguagePropertiesFromMappings createTempWelshPropertiesFromMappings = new CreateTempLanguagePropertiesFromMappings();
 //        createTempWelshPropertiesFromMappings.createTestTempClaimWelshFile();
 //    }
 }
