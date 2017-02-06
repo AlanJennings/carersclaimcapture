7cff831bb47eb6173388a1fac13cfb0aa10ad111
Peter Whitehead <peter@scrumconnect.com>
2017-02-06 13:35:43 +0000

    CADS-1735 : breaks in care

---
+++ src/test/java/uk/gov/dwp/carersallowance/jsp/FunctionsTest.java
@@ -0,0 +1,42 @@
+package uk.gov.dwp.carersallowance.jsp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by peterwhitehead on 04/01/2017.
+ */
+public class FunctionsTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testDateOffset() throws Exception {
+
+    }
+
+    @Test
+    public void testDateOffsetFromCurrent() throws Exception {
+
+    }
+
+    @Test
+    public void testProp() throws Exception {
+
+    }
+
+    @Test
+    public void testEncrypt() throws Exception {
+
+    }
+
+    @Test
+    public void testDecrypt() throws Exception {
+
+    }
+}
